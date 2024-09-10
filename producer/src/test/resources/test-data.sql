7af202a0588cf3c6bbc4eef1de0cf006d0b368c1
ilhwan <poj0722@naver.com>
2024-09-11 01:39:06 +0900

    edit test data

--- producer/src/test/resources/test-data.sql
+++ producer/src/test/resources/test-data.sql
@@ -1,10 +1,10 @@
 INSERT INTO noti_group
-VALUES ('11111111-1111-1111-1111-111111111111', 'test_name1', 'test_desc1', NOW(), NOW()),
-       ('22222222-2222-2222-2222-222222222222', 'test_name2', 'test_desc2', NOW(), NOW());
+VALUES ('11111111-1111-1111-1111-111111111111', 'group_name1', 'test_desc1', NOW(), NOW()),
+       ('22222222-2222-2222-2222-222222222222', 'group_name2', 'test_desc2', NOW(), NOW());
 
 INSERT INTO users
-VALUES ('11111111-1111-1111-1111-111111111111', 'test_name1', 'test_token1', 'test_channel_id1', NOW(), NOW()),
-       ('22222222-2222-2222-2222-222222222222', 'test_name2', 'test_token2', 'test_channel_id2', NOW(), NOW());
+VALUES ('11111111-1111-1111-1111-111111111111', 'user_name1', 'test_token1', 'test_channel_id1', NOW(), NOW()),
+       ('22222222-2222-2222-2222-222222222222', 'user_name2', 'test_token2', 'test_channel_id2', NOW(), NOW());
 
 INSERT INTO noti_group_user
 VALUES ('11111111-1111-1111-1111-111111111111', '11111111-1111-1111-1111-111111111111',
